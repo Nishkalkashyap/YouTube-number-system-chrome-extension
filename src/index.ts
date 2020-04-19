d2a24af7092b91dd14df1fad029ffeafd00ae8ec
Nishkal <kashyapnishkal@gmail.com>
2020-04-19 12:34:19 +0530

    Partially resolved likes bad text issue

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import numeral from 'numeral';
 
 const ONE_LAKH = 100000;
 const ONE_CRORE = 100 * ONE_LAKH;
+const ORIGINAL_INNER_TEXT_ATTRIBUTE = 'original-inner-text';
 
 setInterval(() => {
     restoreSanity();
@@ -20,15 +21,18 @@ function restoreSanity() {
 
     const itemsToChange = allItems.filter((item) => {
         const el: HTMLElement = item as any;
-        return !!el.innerText.match(/(crore|lakh)/i)
+        return (!!el.innerText.match(/(crore|lakh)/i) || el.innerText == '0')
     }).filter((item, index, arr) => {
         return arr.indexOf(item) == index;
     });
 
     itemsToChange.map((item) => {
         const el: HTMLElement = item as any;
-        const text = el.innerText;
-        el.innerText = parseRawText(text);
+        if (!el.getAttribute(ORIGINAL_INNER_TEXT_ATTRIBUTE)) {
+            el.setAttribute(ORIGINAL_INNER_TEXT_ATTRIBUTE, el.innerText);
+        }
+
+        el.innerHTML = parseRawText(el.getAttribute(ORIGINAL_INNER_TEXT_ATTRIBUTE));
     });
     // console.timeEnd('RESTORE_SANITY');
 }
@@ -37,9 +41,14 @@ function parseRawText(text: string) {
     const split = text.split(/\s/g);
     const num = Number(split[0]).valueOf();
     const identifier: 'lakh' | 'crore' = split[1] as any;
+    const endText: 'views' | 'subscribers' = split[2] as any;
 
     const inNumbers = num * (identifier == 'lakh' ? ONE_LAKH : ONE_CRORE);
 
-    const formattedText = numeral(inNumbers).format('0.0 a').toUpperCase().replace(/(\s|\.0)/gi, '');
+    let formattedText = numeral(inNumbers).format('0.0 a').toUpperCase().replace(/(\s|\.0)/gi, '');
+    if (endText) {
+        formattedText = formattedText.concat(` ${endText}`);
+    }
+    formattedText = formattedText.replace(/\s/g, '&nbsp;');
     return formattedText;
 }
